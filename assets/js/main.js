193d2970ec14b75652474e0d8386cc335045a29a
DGNVMusic <dgnvserveryes@gmail.com>
2025-01-12 17:49:27 +0100

    Added Ayame Extension and more

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 "use strict"; // Use strict JavaScript mode to prevent errors.
 $(document).ready(function() {
     console.log("Page loaded in " + (performance.now() / 1000).toFixed(2) + " seconds."); // Log the page load time in seconds.
+
+    document.querySelectorAll('.data-extensionid').forEach(item => {
+        item.addEventListener('click', event => {
+            let extensionId = item.getAttribute('data-extensionid');
+            let url = '/extension/' + extensionId;
+            window.location.href = url;
+        });
+    });
 });
