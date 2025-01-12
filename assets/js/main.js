cf6507ba31bf3c77e42ccc010cc05f91c029ef33
DGNVMusic <dgnvserveryes@gmail.com>
2025-01-12 15:53:03 +0100

    Init

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+"use strict"; // Use strict JavaScript mode to prevent errors.
+$(document).ready(function() {
+    console.log("Page loaded in " + (performance.now() / 1000).toFixed(2) + " seconds."); // Log the page load time in seconds.
+});
